61b02d02a579b6d3bede342f24ca7935ebfcc234
1gy <42576495+1gy@users.noreply.github.com>
2024-04-25 13:46:06 +0000

    classfileのconstant定義を追加

---
+++ crates/rj_core/src/class/constant.rs
@@ -0,0 +1,64 @@
+// class file format
+// https://docs.oracle.com/javase/specs/jvms/se21/html/jvms-4.html
+
+#[derive(Debug)]
+pub enum Constant {
+    Utf8 {
+        value: String,
+    },
+    Integer {
+        value: i32,
+    },
+    Float {
+        value: f32,
+    },
+    Long {
+        value: i64,
+    },
+    Double {
+        value: f64,
+    },
+    Class {
+        name_index: u16,
+    },
+    String {
+        string_index: u16,
+    },
+    Fieldref {
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    Methodref {
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    InterfaceMethodref {
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    NameAndType {
+        name_index: u16,
+        descriptor_index: u16,
+    },
+    MethodHandle {
+        reference_kind: u8,
+        reference_index: u16,
+    },
+    MethodType {
+        descriptor_index: u16,
+    },
+    Dynamic {
+        bootstrap_method_attr_index: u16,
+        name_and_type_index: u16,
+    },
+    InvokeDynamic {
+        bootstrap_method_attr_index: u16,
+        name_and_type_index: u16,
+    },
+    Module {
+        name_index: u16,
+    },
+    Package {
+        name_index: u16,
+    },
+}
