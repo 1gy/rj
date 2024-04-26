6de21b51cf27c7f636cdddd331d07f47c2cc8bbb
1gy <42576495+1gy@users.noreply.github.com>
2024-04-26 14:08:12 +0000

    feat: CONSTANT_Integer, CONSTANT_Float のパース処理を実装

--- crates/rj_core/src/class/constant.rs
+++ crates/rj_core/src/class/constant.rs
@@ -73,6 +73,16 @@ pub fn parse_utf8(input: &[u8]) -> Result<(&[u8], Constant), ClassParseError> {
     Ok((input, Constant::Utf8 { value }))
 }
 
+pub fn parse_integer(input: &[u8]) -> Result<(&[u8], Constant), ClassParseError> {
+    let (input, value) = parser::be_i32(input)?;
+    Ok((input, Constant::Integer { value }))
+}
+
+pub fn parse_float(input: &[u8]) -> Result<(&[u8], Constant), ClassParseError> {
+    let (input, value) = parser::be_f32(input)?;
+    Ok((input, Constant::Float { value }))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -107,5 +117,35 @@ mod tests {
         let input = [0x00, 0x03, 0x41, 0x42, 0x80];
         let result = parse_utf8(&input);
         assert!(matches!(result, Err(ClassParseError::Utf8Error(_))));
+    }
+
+    #[test]
+    fn test_parse_integer() {
+        let input = [0x12, 0x34, 0x56, 0x78];
+        let (rest, constant) = parse_integer(&input).unwrap();
+        assert_eq!(rest, &[]);
+        assert_eq!(constant, Constant::Integer { value: 0x12345678 });
+
+        let input = [0x12, 0x34, 0x56];
+        let result = parse_integer(&input);
+        assert_eq!(
+            result,
+            Err(ClassParseError::ParseError(parser::ParseError::Eof))
+        );
+    }
+
+    #[test]
+    fn test_parse_float() {
+        let input = [0x3f, 0x9d, 0xf3, 0xb6];
+        let (rest, constant) = parse_float(&input).unwrap();
+        assert_eq!(rest, &[]);
+        assert_eq!(constant, Constant::Float { value: 1.234 });
+
+        let input = [0x3f, 0x9d, 0xf3];
+        let result = parse_float(&input);
+        assert_eq!(
+            result,
+            Err(ClassParseError::ParseError(parser::ParseError::Eof))
+        );
     }
 }
